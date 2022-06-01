7568e21f2af94aaea0657c260095d6327ceea6fb
YongJoOoO <39732720+YongJoOoO@users.noreply.github.com>
2022-06-01 15:31:24 +0900

    프로젝트 끝 20220601

---
+++ FinalProject/src/main/java/com/system/pos/beans/DayCommand.java
@@ -0,0 +1,25 @@
+package com.system.pos.beans;
+
+public class DayCommand {
+	
+	//필드 
+	private String start;
+	private String end;
+	
+	//getset
+	public String getStart() {
+		return start;
+	}
+	public void setStart(String start) {
+		this.start = start;
+	}
+	public String getEnd() {
+		return end;
+	}
+	public void setEnd(String end) {
+		this.end = end;
+	}
+	
+	
+	
+}
